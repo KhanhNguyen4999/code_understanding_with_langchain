c16e0e606ca1ed01cea20fae6ad461b99b18cd24
Martin Tran <martin.tran@legato.co>
2023-08-11 22:52:13 +0700

    change UI page search

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,17 +1,28 @@
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, ToastOptions, toast } from "react-toastify";
 import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
-import { endpointSubmitUrl } from "../untils/APIRoutes";
-import { useNavigate } from "react-router-dom";
+import Logo from "../assets/logo.svg";
 import Loading from "../components/Loading";
-import { info_user, access_token } from "./Login";
+import { endpointSubmitUrl } from "../untils/APIRoutes";
+import { access_token, info_user } from "./Login";
+import "react-toastify/dist/ReactToastify.css";
 
 const Search = () => {
   const navigate = useNavigate();
   const inputRef: any = useRef();
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState<any>({});
+
+  const toastOptions: ToastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
 
   useEffect(() => {
     if (!sessionStorage.getItem(access_token)) {
@@ -46,6 +57,7 @@ const Search = () => {
         navigate("/home");
       } catch (error) {
         console.log("error", error);
+        toast.error("Please enter link github", toastOptions);
       }
       setIsLoading(false);
     }
@@ -61,10 +73,13 @@ const Search = () => {
       {isLoading ? (
         <Loading />
       ) : (
-        <Container>
-          <form onSubmit={handleSubmit}>
-            <div className="wrapper">
-              <label className="title">CODE UNDERSTANDING</label>
+        <>
+          <FormContainer>
+            <form onSubmit={handleSubmit}>
+              <div className="brand">
+                <img src={Logo} alt="Logo" />
+                <h1>Search</h1>
+              </div>
               <input
                 ref={inputRef}
                 type="text"
@@ -72,61 +87,86 @@ const Search = () => {
                 placeholder="Please enter URL"
                 className="inputUrl"
               />
-            </div>
-
-            <button type="submit" className="btn">
-              Phân tích
-            </button>
-            <button type="submit" className="btn_logout" onClick={handleLogOut}>
-              Log out
-            </button>
-          </form>
-        </Container>
+              <button type="submit">Submit</button>
+              <span onClick={handleLogOut}>Log out</span>
+            </form>
+          </FormContainer>
+          <ToastContainer />
+        </>
       )}
     </>
   );
 };
 
-const Container = styled.div`
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
-  margin: auto;
-  width: 100%;
-  height: 100vh;
-  h1 {
-    color: white;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
   }
-  .wrapper {
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: auto;
+    font-size: 1rem;
+    width: 300px;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #997af0;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 8px;
-  }
-  .title {
-    font-size: 40px;
-    color: red;
+    color: #4e0eff;
+    text-decoration: none;
     font-weight: bold;
-  }
-  .inputUrl {
-    height: 40px;
-    width: 400px;
-  }
-  .btn,
-  .btn_logout {
-    display: flex;
-    margin: 16px auto;
-    padding: 12px 16px;
-    font-size: 24px;
-    color: #ffff;
-    background: green;
-    border: none;
-    border-radius: 8px;
     cursor: pointer;
-  }
-  .btn_logout {
-    margin-top: 40px;
   }
 `;
 
